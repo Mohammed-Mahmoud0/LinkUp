d593120aed7adc3a0e012169330cb181d31ac2d2
Mohamed Mahmoud <mf.mohamedfawzy0@gmail.com>
2024-08-23 19:35:54 +0300

    project structure and firebase initialized

---
+++ lib/link_up.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LinkUp extends StatelessWidget {
+  const LinkUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LinkUp',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
